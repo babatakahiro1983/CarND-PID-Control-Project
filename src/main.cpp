ce5eca160d1343fe53482c4a8f1195ccba9bc01d
babatakahiro <babatakahiro@gmail.com>
2017-11-21 20:08:06 +0900

    評価関数

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ int main()
           pid.num += 1;	
           pid.error_sum += cte * cte;
 
-          if (num > 0) {
+          if (pid.num > 0) {
           	pid.error_eval = pid.error_sum / pid.num;
           }
 
